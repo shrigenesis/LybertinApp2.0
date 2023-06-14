fb46fe2e25e21526dfa76794bc7418285037494c
Ankit Sharma <ankit.sharma@shrigenesis.com>
2023-06-14 15:26:23 +0530

    Download not workinf every time issue fix

--- src/utils/download.js
+++ src/utils/download.js
@@ -1,102 +1,99 @@
 import RNFetchBlob from 'react-native-blob-util';
 import Toast from 'react-native-toast-message';
 import moment from 'moment';
-import { PermissionsAndroid, Platform } from 'react-native';
-
+import {PermissionsAndroid, Platform} from 'react-native';
 
 export const Download = async (url, ext, setDownloadProgress) => {
-    const granted = Platform.OS !== 'ios' && await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE);
+  const granted =
+    Platform.OS !== 'ios' &&
+    (await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+    ));
 
-    if (url && granted == 'granted') {
-        return new Promise(async (resolve) => {
-            let name = `${moment().unix()}`;
-            const { config, fs } = RNFetchBlob;
-            let PictureDir = fs.dirs.DownloadDir;
-            const dirToSave = Platform.OS == 'ios' ? fs.dirs.DocumentDir : fs.dirs.DownloadDir
-            let options = {
-                fileCache: true,
-                IOSBackgroundTask: true,
-                addAndroidDownloads: {
-                    useDownloadManager: true,
-                    notification: true,
-                    path:
-                        PictureDir + '/' +
-                        `${name}.${ext}`,
-                    description: `${name}.${ext} Download`,
-                },
-                appendExt: 'pdf',
-                useDownloadManager: true,
-                notification: true,
-                mediaScannable: true,
-                title: `${name}.${ext} Download`,
-                path: `${dirToSave}/${name}.${ext}`, 
-            };
-            config(options)
-                .fetch('GET', url)
-                .then(async (res) => {
-                    console.log(res, '--------');
-                    setDownloadProgress && setDownloadProgress(false)
-                    resolve(res);
-                    if (Platform.OS === "ios") {
-                        RNFetchBlob.ios.previewDocument('file://' + res.path());
-                    }
-                    Toast.show({
-                        type: 'info',
-                        text1: 'File Downloaded!'
-                    })
-                }).catch((err) => {
-                    console.log(err)
-                });
+  if (url && granted == 'granted') {
+    return new Promise(async resolve => {
+      let name = `${moment().unix()}`;
+      const {config, fs} = RNFetchBlob;
+      let PictureDir = fs.dirs.DownloadDir;
+      const dirToSave =
+        Platform.OS == 'ios' ? fs.dirs.DocumentDir : fs.dirs.DownloadDir;
+      let options = {
+        fileCache: true,
+        IOSBackgroundTask: true,
+        addAndroidDownloads: {
+          useDownloadManager: true,
+          notification: true,
+          path: PictureDir + '/' + `${name}.${ext}`,
+          description: `${name}.${ext} Download`,
+        },
+        appendExt: ext,
+        useDownloadManager: true,
+        notification: true,
+        mediaScannable: true,
+        title: `${name}.${ext} Download`,
+        path: `${dirToSave}/${name}.${ext}`,
+      };
+      config(options)
+        .fetch('GET', url)
+        .then(async res => {
+          setDownloadProgress && setDownloadProgress(false);
+          resolve(res);
+          if (Platform.OS === 'ios') {
+            RNFetchBlob.fs.writeFile(options.path, res.data, 'base64');
+            RNFetchBlob.ios.previewDocument(options.path);
+          }
+          Toast.show({
+            type: 'info',
+            text1: 'File Downloaded!',
+          });
         })
-    }
-    else {
-        return new Promise(async (resolve) => {
-            let name = `${moment().unix()}`;
-            const { config, fs } = RNFetchBlob;
-            let PictureDir = fs.dirs.DownloadDir;
-            const dirToSave = Platform.OS == 'ios' ? fs.dirs.DocumentDir : fs.dirs.DownloadDir
-            let options = {
-                fileCache: true,
-                IOSBackgroundTask: true,
-                addAndroidDownloads: {
-                    useDownloadManager: true,
-                    notification: true,
-                    path:
-                        PictureDir + '/' +
-                        `${name}.${ext}`,
-                    description: `${name}.${ext} Download`,
-                },
-                appendExt: 'pdf',
-                useDownloadManager: true,
-                notification: true,
-                mediaScannable: true,
-                title: `${name}.${ext} Download`,
-                path: `${dirToSave}/${name}.${ext}`,
-            };
-            config(options)
-                .fetch('GET', url)
-                .progress((received, total) => {
-                    console.log(received, total);
-                })
-                .then(async (res) => {
-                    console.log(res, "res");
-                    setDownloadProgress && setDownloadProgress(false)
-                    resolve(res);
-                    if (Platform.OS === "ios") {
-                        // RNFetchBlob.fs.writeFile(`${dirToSave}/${name}.${ext}`, res.data, 'base64');
-                        // RNFetchBlob.ios.previewDocument(`${dirToSave}/${name}.${ext}`);
-                        RNFetchBlob.ios.previewDocument('file://' + res.path());
-                    }
-                    Toast.show({
-                        type: 'info',
-                        text1: 'File Downloaded!'
-                    })
-                }).catch((err) => {
-                    console.log(err)
-                });
+        .catch(err => {
+          console.log(err);
+        });
+    });
+  } else {
+    return new Promise(async resolve => {
+      let name = `${moment().unix()}`;
+      const {config, fs} = RNFetchBlob;
+      let PictureDir = fs.dirs.DownloadDir;
+      const dirToSave =
+        Platform.OS == 'ios' ? fs.dirs.DocumentDir : fs.dirs.DownloadDir;
+      let options = {
+        // fileCache: true,
+        IOSBackgroundTask: true,
+        addAndroidDownloads: {
+          useDownloadManager: true,
+          notification: true,
+          path: PictureDir + '/' + `${name}.${ext}`,
+          description: `${name}.${ext} Download`,
+        },
+        appendExt: ext,
+        useDownloadManager: true,
+        notification: true,
+        mediaScannable: true,
+        title: `${name}.${ext} Download`,
+        path: `${dirToSave}/${name}.${ext}`,
+      };
+      config(options)
+        .fetch('GET', url)
+        .progress((received, total) => {
+          console.log(received, total);
         })
-    }
-
-}
-
-
+        .then(async res => {
+          setDownloadProgress && setDownloadProgress(false);
+          resolve(res);
+          if (Platform.OS === 'ios') {
+            RNFetchBlob.fs.writeFile(options.path, res.data, 'base64');
+            RNFetchBlob.ios.previewDocument(options.path);
+          }
+          Toast.show({
+            type: 'info',
+            text1: 'File Downloaded!',
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+  }
+};
